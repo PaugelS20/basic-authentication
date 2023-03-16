734a6a0154d08e82258f1db7234c54c521584776
Sam Paugel <shpaugel@madisoncollege.edu>
2023-03-15 20:18:34 -0500

    trying to solve the no ui mystery

--- src/Nav.js
+++ src/Nav.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Menu } from 'antd';
-import { 
-    HomeOutlined, 
-    ProfileOutlined, 
-    FileProtectOutlined 
-} from '@ant-design/icons';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
+import {
+	HomeOutlined,
+	ProfileOutlined,
+	FileProtectOutlined,
+} from "@ant-design/icons";
 
-export const Nav = ({current}) => {
-  return (
-    <div>
-      <Menu selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key='home'>
-          <Link to={`/`}>
-            <HomeOutlined />Home
-          </Link>
-        </Menu.Item>
-        
-        <Menu.Item key='profile'>
-          <Link to='/profile'>
-          <ProfileOutlined />Profile
-          </Link>
-        </Menu.Item>
-        
-        <Menu.Item key='protected'>
-          <Link to='/protected'>
-            <FileProtectOutlined />Protected
-          </Link>
-        </Menu.Item>
-      </Menu>
-    </div>
-  )
-}
+export const Nav = ({ current }) => {
+	return (
+		<div>
+			<Menu selectedKeys={[current]} mode="horizontal">
+				<Menu.Item key="home">
+					<Link to={`/`}>
+						<HomeOutlined />
+						Home
+					</Link>
+				</Menu.Item>
+
+				<Menu.Item key="profile">
+					<Link to="/profile">
+						<ProfileOutlined />
+						Profile
+					</Link>
+				</Menu.Item>
+
+				<Menu.Item key="protected">
+					<Link to="/protected">
+						<FileProtectOutlined />
+						Protected
+					</Link>
+				</Menu.Item>
+			</Menu>
+		</div>
+	);
+};
