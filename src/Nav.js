3cba5f57ee096f2fda108084d5bce7e5db84ccb1
Sam Paugel <shpaugel@madisoncollege.edu>
2023-03-17 17:21:30 -0500

    updated to newer syntax

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "antd";
 import {
@@ -7,32 +7,57 @@ import {
 	FileProtectOutlined,
 } from "@ant-design/icons";
 
-const Nav = ({ current }) => {
-	return (
-		<div>
-			<Menu selectedKeys={[current]} mode="horizontal">
-				<Menu.Item key="home">
-					<Link to={`/`}>
-						<HomeOutlined />
-						Home
-					</Link>
-				</Menu.Item>
+const Nav = () => {
+	const items = [
+		{
+			label: <Link to={`/`}>Home</Link>,
+			key: "home",
+			icon: <HomeOutlined />,
+		},
+		{
+			label: <Link to={`/profile`}>Profile</Link>,
+			key: "profile",
+			icon: <ProfileOutlined />,
+		},
+		{
+			label: <Link to={`/protected`}>Protected</Link>,
+			key: "protected",
+			icon: <FileProtectOutlined />,
+		},
+	];
 
-				<Menu.Item key="profile">
-					<Link to="/profile">
-						<ProfileOutlined />
-						Profile
-					</Link>
-				</Menu.Item>
+	const [current] = useState();
 
-				<Menu.Item key="protected">
-					<Link to="/protected">
-						<FileProtectOutlined />
-						Protected
-					</Link>
-				</Menu.Item>
-			</Menu>
-		</div>
-	);
+	return <Menu selectedKeys={[current]} mode="horizontal" items={items} />;
 };
 export default Nav;
+
+// <div>
+// 	<Menu selectedKeys={[current]} mode="horizontal">
+// 		<Menu.Item key="home">
+// 			<Link to={`/`}>
+// 				<HomeOutlined />
+// 				Home
+// 			</Link>
+// 		</Menu.Item>
+
+// 		<Menu.Item key="profile">
+// 			<Link to="/profile">
+// 				<ProfileOutlined />
+// 				Profile
+// 			</Link>
+// 		</Menu.Item>
+
+// 		<Menu.Item key="protected">
+// 			<Link to="/protected">
+// 				<FileProtectOutlined />
+// 				Protected
+// 			</Link>
+// 		</Menu.Item>
+// 	</Menu>
+// </div>
+// return (
+// 	<div>
+// 		<Menu selectedKeys={[current]} mode="horizontal"></Menu>
+// 	</div>
+// );
