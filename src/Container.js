b70dc9ec81d3115f60cdceb2f3aeeabebe9f6a57
Sam Paugel <shpaugel@madisoncollege.edu>
2023-03-17 17:16:03 -0500

    old vrs need to figure out how to update

--- src/Container.js
+++ src/Container.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+// children will be removed in the next major upadte of ant design neet to change to items somhow
 const Container = ({ children }) => (
   <div style={styles.container}>
     { children }
   </div>
-)
+);
 
 const styles = {
   container: {
